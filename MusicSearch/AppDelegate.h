aa2fec7031886eb42b1733ea15c722344e3d383a
Vijay Karthik <jvijaykarthik@gmail.com>
2016-11-19 08:31:31 -0500

    Initial Commit

---
+++ MusicSearch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MusicSearch
+//
+//  Created by Vijay Karthik on 11/19/16.
+//  Copyright © 2016 Vijay Karthik Jeyaraj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MusicSearch/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MusicSearch
+//
+//  Created by Vijay Karthik on 11/19/16.
+//  Copyright © 2016 Vijay Karthik Jeyaraj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
